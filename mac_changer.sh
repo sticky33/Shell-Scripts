abd11d22568177c4adcdad30eab7a2c04e47a709
sticky <48205991+sticky33@users.noreply.github.com>
2020-11-07 11:13:01 +0530

    Update mac_changer.sh

--- mac_changer.sh
+++ mac_changer.sh
@@ -4,10 +4,14 @@
 echo "A simple Mac_Changer
 ----------------------------------------"
 read -p "Enter your interface:" interface
+function current_mac(){
+ifconfig $interface | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'
+}
+echo "Your current mac is $(current_mac)"
 read -p "Enter the new mac-address:" mac
 
 sudo ifconfig $interface down
 sudo ifconfig $interface hw ether $mac
 sudo ifconfig $interface up
 
-echo "mac_address changed successfully to $mac"
+echo "mac_address changed successfully changed to $mac"
